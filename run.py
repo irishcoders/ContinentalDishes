48777cc9c7f95ed23f2c21882eab4a850bdd2c4c
irishcoders <>
2023-10-09 09:13:26 +0000

    final commit

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+ import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('cred.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('continental_dishes')
+
+customer_orders = SHEET.worksheet('customer_orders')
+
 # This is the list of menu and prices to be displayed to the user/customer
 menu = {
     1: {"item": "Jollof Rice", "price": 40},
