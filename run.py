6c9893efe21063f5000a0acea76603a48de6592a
irishcoders <>
2023-10-09 06:25:57 +0000

    add menu list and prices

--- run.py
+++ run.py
@@ -14,6 +14,16 @@ SHEET = GSPREAD_CLIENT.open('continental_dishes')
 
 customer_orders = SHEET.worksheet('customer_orders')
 
-data = customer_orders.get_all_values()
-print(data)
-
+# This is the list of menu and prices to be displayed to the user/customer
+menu = {
+    1: {"item": "Jollof Rice", "price": 40},
+    2: {"item": "Beef Pepper-soup", "price": 35},
+    3: {"item": "Prawn Cocktail", "price": 25},
+    4: {"item": "Spaghetti Carbonara", "price": 30},
+    5: {"item": "French fries", "price": 15},
+    6: {"item": "Buffalo Wings", "price": 22},
+    7: {"item": "Macaroni and Cheese", "price": 28},
+    8: {"item": "Coca-Cola", "price": 10},
+    9: {"item": "Fanta", "price": 10},
+    10: {"item": "Bottled Water", "price": 7}
+}
